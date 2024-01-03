d90f72263edf0f270c61455aaffe2f4c9a410632
MOYNUL75 <mmoynulislam2000@gmail.com>
2024-01-03 13:36:42 +0600

    Add Machine Comment

--- Hello.cpp
+++ Hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
     cout<<"Hello World"<<endl;
-    cout<<"This is a new feature"<<endl;
+    cout<<"This is a new feature for this machine"<<endl;
     return 0;
 }
